f1e3b81474cb7bddcf4d5d9123b0e9f2c14748d6
CrazyFox-001 <46416788+CrazyFox-001@users.noreply.github.com>
2019-01-10 22:33:48 +0530

    Update idprediction.py

--- idprediction.py
+++ idprediction.py
@@ -2,13 +2,13 @@ import numpy as np
 from keras.models import  load_model
 from keras.preprocessing import image
 
-testimage = image.load_img('predict/12.jpg',target_size=(64,64))
+testimage = image.load_img('predict/16.jpg',target_size=(64,64))
 
 testimage = image.img_to_array(testimage)
 testimage = np.expand_dims(testimage,0)
-#testimage  = testimage.astype('float32')
+#testimage  = testimage.astype('float64')
 testimage /= 255
-model = load_model('ids.h5')
+model = load_model('nic.h5')
 results =model.predict(testimage)
 
 preds = model.predict_proba(testimage)
@@ -22,7 +22,10 @@ nic_front = preds[0][2]
 nic_back = preds[0][1]
 license  = preds[0][0]
 
-precentage = license/final_score *100
+precentagenicf = nic_front/final_score *100
+precentagenicb = nic_back/final_score *100
+precentagelic = license/final_score *100
+
 
 if (pred_classes == 0):
     print("License front")
@@ -31,7 +34,10 @@ elif (pred_classes == 1):
 elif (pred_classes == 2):
     print("NIC Front")
 print("prediction",preds)
-print("precentage",precentage)
+print("precentage nic front ",precentagenicf)
+print("precentage nic back",precentagenicb)
+print("precentage license",precentagelic)
+
 
 
 
