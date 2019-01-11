2a53091d66082809ea45f8a4e7e71ee6eb24a07a
lmadhuranga <lilan.maduranga@gmail.com>
2019-01-11 19:02:56 +0530

    added aditional print for undestand

--- nic_rename_using_predict.py
+++ nic_rename_using_predict.py
@@ -33,13 +33,14 @@ def preds():
             testimage /= 255
             model = load_model(projectPath+ '/trainedModles/nICTrainedModelVNic700.h5')
             preds = model.predict_proba(testimage)
-            print('preds', preds)
+            # print('                   lic_front', '     nic_back', '     nic_front')
+            # print('preds', preds)
             pred_classes = np.argmax(preds)
-            print("pred_classes", pred_classes)
+            # print("pred_classes", pred_classes)
             final_score = preds[0][0] + preds[0][1] + preds[0][2]
-            print("final_score", final_score)
+            # print("final_score", final_score)
 
-        yield pred_classes, f
+        yield pred_classes, f, preds, final_score
 
 
 preds()
@@ -48,33 +49,48 @@ counterNicFront = 0
 counterNicBack = 0
 counterNicLicence = 0
 counterOther = 0
-#
-for pred_classes, f in preds():
+
+for pred_classes, f, preds, final_score in preds():
+    # print('*********************************************************************')
     randomno = random.randint(1, 90000)
+    print('                  lic_front', '     nic_back', '      nic_front')
+    print('preds', preds)
+    print("pred_classes", pred_classes)
+    print("final_score", final_score)
+    if(final_score>0.6):
+        if pred_classes == 0:
+            # counterNicLicence += 1
+            # img_path = projectPath + '/output/licence_front/licence_front_' + `counterNicLicence` + '.jpg'
+            # os.rename(f, img_path)
+            # print('img_path', img_path)
+            print('file',f)
+            print("License_front")
 
-    if pred_classes == 0:
-        counterNicLicence += 1
-        img_path = projectPath + '/output/licence_front/licence_front_' + `counterNicLicence` + '.jpg'
-        os.rename(f, img_path)
-        print('img_path', img_path)
-        print("License_front")
+        elif pred_classes == 1:
+            # counterNicBack += 1
+            # img_path = projectPath + '/output/nic_back/nic_back_' + `counterNicBack` + '.jpg'
+            # os.rename(f, img_path)
+            # print('img_path', img_path)
+            print('file',f)
+            print("NIC Back")
 
-    elif pred_classes == 1:
-        counterNicBack += 1
-        img_path = projectPath + '/output/nic_back/nic_back_' + `counterNicBack` + '.jpg'
-        os.rename(f, img_path)
-        print('img_path', img_path)
-        print("NIC Back")
+        elif pred_classes == 2:
+            # counterNicFront += 1
+            # img_path = projectPath + '/output/nic_front/nic_front_' + `counterNicFront` + '.jpg'
+            # os.rename(f, img_path)
+            # print('img_path', img_path)
+            print('file',f)
+            print("NIC Front")
 
-    elif pred_classes == 2:
-        counterNicFront += 1
-        img_path = projectPath + '/output/nic_front/nic_front_' + `counterNicFront` + '.jpg'
-        os.rename(f, img_path)
-        print('img_path', img_path)
-        print("NIC Front")
-
+        else:
+            counterOther += 1
+            print('file',f)
+            print("Other image 1")
     else:
         counterOther += 1
+        print('file', f)
+        print("Other image 2 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+    print('+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++')
 
 print('---------------------- Summery --------------------')
 print('counterNicFront', counterNicFront)
